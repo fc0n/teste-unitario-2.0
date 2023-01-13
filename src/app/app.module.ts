99b08c72fa7a8c424c052287efc3512c05488e41
Jean Gabriel <jeangabrieljb9@gmail.com>
2023-01-13 00:17:52 -0300

    focando em um único teste -fdescribe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FixProblemsComponent } from './fix-problems/fix-problems.component';
+import { DebuggerTestsComponent } from './debugger-tests/debugger-tests.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FixProblemsComponent,
+    DebuggerTestsComponent,
   ],
   imports: [
     BrowserModule,
