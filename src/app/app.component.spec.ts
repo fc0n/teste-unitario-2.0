ebd872bb75815eb55ed3b18f96bcca9569d9f20d
Jean Gabriel <jeangabrieljb9@gmail.com>
2023-01-13 01:05:11 -0300

    CUSTOM_ELEMENTS_SCHEMA

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
@@ -13,6 +15,9 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
       ],
     }).compileComponents();
 
