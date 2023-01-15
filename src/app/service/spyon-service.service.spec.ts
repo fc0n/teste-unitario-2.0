345af2ad801a9d83ccabb92e3f62dc3825c4923b
Jean Gabriel <jeangabrieljb9@gmail.com>
2023-01-15 16:22:59 -0300

    espionando métodos de serviços com spyOn

---
+++ src/app/service/spyon-service.service.spec.ts
@@ -0,0 +1,32 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+import { SpyonServiceService } from './spyon-service.service';
+
+describe('SpyonServiceService', () => {
+  let service: SpyonServiceService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(SpyonServiceService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return a list of users', () => {
+    const response = [
+      {name: 'Jean', age: 30, email: 'jean.gabriel@gmail.com'},
+      {name: 'Jose', age: 40, email: 'jose.silva@gmail.com'},
+      {name: 'Maria', age: 22, email: 'maria.silva@gmail.com'},
+    ]
+    spyOn(service, 'getUsers').and.returnValue(of(response))
+    service.getUsers().subscribe(res => {
+      expect(res).toEqual(response);
+    })
+  });
+});
